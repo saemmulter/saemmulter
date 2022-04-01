7c8fed8484b021107b53febc84b2708cfc80aea1
Chloe <gtalk.with.nani@gmail.com>
2022-04-01 14:56:38 +0900

    Created error page
    404 - Page Not Found
    500 - Server-side error occurred

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+function Custom404() {
+  return (
+    <>
+      <h1>404 - Page Not Found</h1>
+    </>
+  )
+}
+
+export default Custom404;
---
+++ pages/500.js
@@ -0,0 +1,9 @@
+function Custom500() {
+  return (
+    <>
+      <h1>500 - Server-side error occurred</h1>
+    </>
+  )
+}
+
+export default Custom500;
