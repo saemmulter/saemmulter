6b2cd22b6d2728d2b7372b26b1110839dfb4d6e7
Chloe <gtalk.with.nani@gmail.com>
2022-05-04 17:11:19 +0900

    Update `Partners` of `Company` contents
    - 샘물터 메뉴의 파트너사 목록 업데이트 (더씨컴퍼니 추가)

--- components/pages/company/partners.js
+++ components/pages/company/partners.js
@@ -12,6 +12,7 @@ const SaeonKoSource = `/company.logo.saeon.ko.png?${(Math.random() * 7).toString
 const KwaterKoSource = `/company.logo.kwater.ko.png?${(Math.random() * 7).toString(7)}`
 const InnoplusKoSource = `/company.logo.innoplus.ko.png?${(Math.random() * 7).toString(7)}`
 const KmtKoSource = `/company.logo.kmt.ko.png?${(Math.random() * 7).toString(7)}`
+const TheseacompanyKoSource = `/company.logo.theseacompany.ko.png?${(Math.random() * 7).toString(7)}`
 
 const AmmonteckEnSource = `/company.logo.ammonteck.en.png?${(Math.random() * 7).toString(7)}`
 const BakertillyEnSource = `/company.logo.bakertilly.en.png?${(Math.random() * 7).toString(7)}`
@@ -22,6 +23,7 @@ const SaeonEnSource = `/company.logo.saeon.en.png?${(Math.random() * 7).toString
 const KwaterEnSource = `/company.logo.kwater.en.png?${(Math.random() * 7).toString(7)}`
 const InnoplusEnSource = `/company.logo.innoplus.en.png?${(Math.random() * 7).toString(7)}`
 const KmtEnSource = `/company.logo.kmt.en.png?${(Math.random() * 7).toString(7)}`
+const TheseacompanyEnSource = `/company.logo.theseacompany.en.png?${(Math.random() * 7).toString(7)}`
 
 const PartnersList = styled.ul({
   '& i': {
@@ -71,6 +73,10 @@ const ImageLogoKmt = styled.i(({ isKorean }) => ({
   backgroundImage: isKorean ? `url(${KmtKoSource})` : `url(${KmtEnSource})`,
 }));
 
+const ImageLogoTheseacompany = styled.i(({ isKorean }) => ({
+  backgroundImage: isKorean ? `url(${TheseacompanyKoSource})` : `url(${TheseacompanyEnSource})`,
+}));
+
 function CompanyPartners() {
   const { i18n, t } = useTranslation('common')
 
@@ -91,6 +97,7 @@ function CompanyPartners() {
       <li><ImageLogoKwater isKorean={isKorean} aria-label={t('partners.org7')} /></li>
       <li><ImageLogoInnoplus isKorean={isKorean} aria-label={t('partners.org8')} /></li>
       <li><ImageLogoKmt isKorean={isKorean} aria-label={t('partners.org9')} /></li>
+      <li><ImageLogoTheseacompany isKorean={isKorean} aria-label={t('partners.org10')} /></li>
     </PartnersList>
   )
 }
