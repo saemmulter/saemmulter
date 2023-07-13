6eeb15216fa15725ec21efaac4faeaf135c4cf71
Chloe <gtalk.with.nani@gmail.com>
2023-07-13 22:09:00 +0900

    App 세팅 설정 변경
    - Server warning 수정
    - i18n 설정 변경
    - react-slick 패키지 버전 올리기

--- components/layout/globalNavigation.js
+++ components/layout/globalNavigation.js
@@ -19,8 +19,10 @@ const NavigationBar = styled.nav(({ open }) => ({
   },
 }));
 
-const MenuLink = styled(LinkButton)(({ currentRouter }) => ({
-  color: currentRouter ? colors.accent : colors.default,
+const MenuLink = styled.li(({ currentRouter }) => ({
+  '& a': {
+    color: currentRouter ? colors.accent : colors.default,
+  },
 }));
 
 const BlogLink = styled.a({
@@ -37,9 +39,11 @@ const Dummy = styled.div({
   ...mixin.colAuto,
 });
 
-const LinkLanguage = styled.a(({ isKorean }) => ({
-  fontWeight: isKorean ? fontWeights.black : fontWeights.regular,
-  opacity: isKorean ? 1 : 0.7
+const LinkLanguage = styled.li(({ isKorean }) => ({
+  '& a': {
+    fontWeight: isKorean ? fontWeights.black : fontWeights.regular,
+    opacity: isKorean ? 1 : 0.7,
+  },
 }));
 
 function GlobalNavigation({ open, setOpen }) {
@@ -61,75 +65,68 @@ function GlobalNavigation({ open, setOpen }) {
       </div>
       <Dummy />
       <ol onClick={() => setOpen(!open)}>
-        <li>
-          <MenuLink
+        <MenuLink currentRouter={router.pathname === '/company' ? true : false}>
+          <LinkButton
             href={menuCompany}
-            currentRouter={router.pathname === '/company' ? true : false}
             aria-label={router.pathname === '/company' ? t('header.active') : null}
           >
             {t('header.company')}
-          </MenuLink>
-        </li>
-        <li>
-          <MenuLink
+          </LinkButton>
+        </MenuLink>
+        <MenuLink currentRouter={router.pathname === '/products' ? true : false}>
+          <LinkButton
             href={menuProducts}
-            currentRouter={router.pathname === '/products' ? true : false}
             aria-label={router.pathname === '/products' ? t('header.active') : null}
           >
             {t('header.products')}
-          </MenuLink>
-        </li>
+          </LinkButton>
+        </MenuLink>
         {i18n.language === 'ko' ? <li><BlogLink href={menuContents}>콘텐츠</BlogLink></li> : null}
-        <li>
-          <MenuLink
+        <MenuLink currentRouter={router.pathname === '/pr-center' ? true : false}>
+          <LinkButton
             href={menuPrCenter}
-            currentRouter={router.pathname === '/pr-center' ? true : false}
             aria-label={router.pathname === '/pr-center' ? t('header.active') : null}
           >
             {t('header.pr-center')}
-          </MenuLink>
-        </li>
-        <li>
-          <MenuLink
+          </LinkButton>
+        </MenuLink>
+        <MenuLink currentRouter={router.pathname === '/downloads' ? true : false}>
+          <LinkButton
             href={menuDownloads}
-            currentRouter={router.pathname === '/downloads' ? true : false}
             aria-label={router.pathname === '/downloads' ? t('header.active') : null}
           >
             {t('header.downloads')}
-          </MenuLink>
-        </li>
-        <li>
-          <MenuLink
+          </LinkButton>
+        </MenuLink>
+        <MenuLink currentRouter={router.pathname === '/contact-us' ? true : false}>
+          <LinkButton
             href={menuContactUs}
-            currentRouter={router.pathname === '/contact-us' ? true : false}
             aria-label={router.pathname === '/contact-us' ? t('header.active') : null}
           >
             {t('header.contact-us')}
-          </MenuLink>
-        </li>
+          </LinkButton>
+        </MenuLink>
       </ol>
       <Dummy />
       <ul>
-        <li>
-          <LinkLanguage
+        <LinkLanguage isKorean={i18n.language === 'ko' ? true : false}>
+          <LinkButton
             href={`/ko${router.pathname}`}
             locale='ko'
             aria-label={i18n.language === 'ko' ? '한국어 선택 / Choose Korean' : 'Choose Korean / 한국어 선택'}
-            isKorean={i18n.language === 'ko' ? true : false}
           >
             Ko
-          </LinkLanguage>
-        </li>
-        <li>
-          <LinkLanguage
+          </LinkButton>
+        </LinkLanguage>
+        <LinkLanguage isKorean={i18n.language === 'ko' ? false : true}>
+          <LinkButton
+            aria-label={i18n.language === 'ko' ? '영어 선택 / Choose English' : 'Choose English / 영어 선택'}
             href={`/en${router.pathname}`}
             locale='en'
-            aria-label={i18n.language === 'ko' ? '영어 선택 / Choose English' : 'Choose English / 영어 선택'}
-            isKorean={i18n.language === 'ko' ? false : true}
           >
             En
-          </LinkLanguage>
-        </li>
+          </LinkButton>
+        </LinkLanguage>
       </ul>
     </NavigationBar>
   )
